2601b363d83ef0b95c4c78d8aab4d31102a408d9
jhhg0 <jhherrera@ccss.sa.cr>
2022-06-10 20:01:32 -0500

    add

--- src/components/CrudApi.js
+++ src/components/CrudApi.js
@@ -2,24 +2,32 @@ import React, { useState, useEffect } from 'react';
 import { helpHttp } from '../helpers/helpHttp';
 import CrudForm from './CrudForm';
 import CrudTable from './CrudTable';
+import Loader from './Loader';
+import Message from './Message';
 
 const CrudApi = () => {
-  const [db, setDb] = useState([]);
+  const [db, setDb] = useState(null);
   const [dataToEdit, setDataToEdit] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
 
   let api = helpHttp();
   let url = 'http://localhost:5000/santos';
 
   useEffect(() => {
+    setLoading(true);
     api.get(url).then((res) => {
       // console.log(res);
       if (!res.err) {
         setDb(res);
+        setError(null);
       } else {
         setDb(null);
+        setError(res);
       }
+      setLoading(false);
     });
-  }, []);
+  }, [url]);
 
   const createData = (data) => {
     data.id = Date.now();
@@ -50,11 +58,21 @@ const CrudApi = () => {
           dataToEdit={dataToEdit}
           setDataToEdit={setDataToEdit}
         />
-        <CrudTable
-          data={db}
-          setDataToEdit={setDataToEdit}
-          deleteData={deleteData}
-        />
+        {loading && <Loader />}
+        {error && (
+          <Message
+            msg={`Error ${error.status}:${error.statusText}`}
+            bgColor='#dc3545'
+          />
+        )}
+
+        {db && (
+          <CrudTable
+            data={db}
+            setDataToEdit={setDataToEdit}
+            deleteData={deleteData}
+          />
+        )}
       </article>
     </div>
   );
